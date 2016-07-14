3cc013c1ee1a1864d1cca72f74f6796ee1572144
SBKubric <sbkubric@gmail.com>
2016-07-15 01:42:13 +0300

    Have added the buttons and the basic description to the CollectionActivity.java and to the NowPlayingActivity.java

--- app/src/main/java/com/sbkubric/android/musicalstructureapp/CollectionActivity.java
+++ app/src/main/java/com/sbkubric/android/musicalstructureapp/CollectionActivity.java
@@ -1,16 +1,23 @@
 package com.sbkubric.android.musicalstructureapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class CollectionActivity extends AppCompatActivity {
+public class CollectionActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((Button) findViewById(R.id.btn_now_playing)).setOnClickListener(this);
+        ((Button) findViewById(R.id.btn_playlist)).setOnClickListener(this);
+        ((Button) findViewById(R.id.btn_collection)).setOnClickListener(this);
+
     }
 
     @Override
@@ -19,6 +26,26 @@ public class CollectionActivity extends AppCompatActivity {
             default:
                 finish();
                 return true;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            default:
+                break;
+            case R.id.btn_now_playing:
+                Intent intentNP = new Intent(this, NowPlayingActivity.class);
+                startActivity(intentNP);
+                break;
+            case R.id.btn_playlist:
+                Intent intentP = new Intent(this, PlaylistActivity.class);
+                startActivity(intentP);
+                break;
+            case R.id.btn_collection:
+                Intent intentC = new Intent(this, CollectionActivity.class);
+                startActivity(intentC);
+                break;
         }
     }
 }
