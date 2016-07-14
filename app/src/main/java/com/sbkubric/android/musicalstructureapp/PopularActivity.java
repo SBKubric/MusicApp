698a3f284904f61e3094d55cdfd82358143df66f
SBKubric <sbkubric@gmail.com>
2016-07-14 11:44:05 +0300

    Initial commit

---
+++ app/src/main/java/com/sbkubric/android/musicalstructureapp/PopularActivity.java
@@ -0,0 +1,13 @@
+package com.sbkubric.android.musicalstructureapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopularActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popular);
+    }
+}
